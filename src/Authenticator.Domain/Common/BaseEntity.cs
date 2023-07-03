ef3a548ab46dace23550e857f9bc2b8c8db20b61
Vasile Cudalb <cudalb.vasile@gmail.com>
2023-07-04 00:23:12 +0300

    [task] #14: Create database entities (#15)

---
+++ src/Authenticator.Domain/Common/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace Authenticator.Domain.Common;
+
+/// <summary>
+///     Represents the base entity that provides default properties for all entities.
+/// </summary>
+public abstract class BaseEntity
+{
+    public Guid Id { get; protected set; }
+}
---
+++ src/Authenticator.Domain/Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Authenticator.Domain.Common;
+
+namespace Authenticator.Domain.Entities;
+
+[ExcludeFromCodeCoverage]
+public class Country : BaseAuditableEntity
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string PhoneCode { get; set; }
+    
+    public ICollection<Address> Addresses { get; set; }
+}
