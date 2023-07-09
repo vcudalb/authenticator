9fccfa9e2db27571c47d1dd6a1f57cd38eea6a6f
Vasile Cudalb <cudalb.vasile@gmail.com>
2023-07-10 00:41:37 +0300

    Implement migration mechanism (#19)

---
+++ src/Authenticator.DbMigrator/Data/Seeds.cs
@@ -0,0 +1,111 @@
+﻿using Duende.IdentityServer.EntityFramework.DbContexts;
+using Duende.IdentityServer.EntityFramework.Mappers;
+using Duende.IdentityServer.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+using Authenticator.Domain.Entities;
+using Authenticator.Infrastructure.Persistence.Contexts;
+
+namespace Authenticator.DbMigrator.Data;
+
+/// <summary>
+///     Db seeds provider service.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public static class Seeds
+{
+    public static void InitializeIdentityDatabase(IServiceProvider serviceProvider)
+    {
+        using (var context = serviceProvider.GetRequiredService<PersistedGrantDbContext>())
+        {
+            context.Database.Migrate();
+
+            Console.WriteLine($"{nameof(PersistedGrantDbContext)} database migrated successfully");
+        }
+
+        using (var context = serviceProvider.GetRequiredService<ConfigurationDbContext>())
+        {
+            context.Database.Migrate();
+
+            SeedClients(context, AuthenticatorConfigData.GetClients());
+            SeedApiResources(context, AuthenticatorConfigData.GetApiResources());
+            SeedApiScope(context, AuthenticatorConfigData.GetApiScopes());
+
+            Console.WriteLine($"{nameof(ConfigurationDbContext)} database migrated successfully");
+        }
+    }
+
+    public static void InitializeAuthDatabase(IServiceProvider serviceProvider)
+    {
+        using var context = serviceProvider.GetRequiredService<AuthenticatorDbContext>();
+        context.Database.Migrate();
+        SeedCountries(context, AuthenticatorConfigData.GetCountries());
+
+        Console.WriteLine($"{nameof(AuthenticatorDbContext)} database migrated successfully");
+    }
+
+    private static void SeedClients(ConfigurationDbContext context, IEnumerable<Client> clients)
+    {
+        if (!context.Clients.Any())
+        {
+            foreach (var client in clients)
+            {
+                context.Clients.Add(client.ToEntity());
+            }
+
+            context.SaveChanges();
+        }
+        else
+        {
+            foreach (var client in clients)
+            {
+                var dbClient = context.Clients.FirstOrDefault(c => c.ClientId == client.ClientId);
+                if (dbClient is not null)
+                {
+                    dbClient.AccessTokenLifetime = client.AccessTokenLifetime;
+                    dbClient.RefreshTokenExpiration = (int)client.RefreshTokenExpiration;
+                    dbClient.SlidingRefreshTokenLifetime = client.SlidingRefreshTokenLifetime;
+                    dbClient.AbsoluteRefreshTokenLifetime = client.AbsoluteRefreshTokenLifetime;
+                }
+            }
+
+            context.SaveChanges();
+        }
+    }
+
+    private static void SeedApiResources(ConfigurationDbContext context, IEnumerable<ApiResource> apiResources)
+    {
+        if (context.ApiResources.Any()) return;
+        foreach (var apiResource in apiResources)
+        {
+            context.ApiResources.Add(apiResource.ToEntity());
+        }
+
+        context.SaveChanges();
+    }
+
+    private static void SeedApiScope(ConfigurationDbContext context, IEnumerable<ApiScope> apiScopes)
+    {
+        if (context.ApiScopes.Any()) return;
+        foreach (var apiScope in apiScopes)
+        {
+            context.ApiScopes.Add(apiScope.ToEntity());
+        }
+
+        context.SaveChanges();
+    }
+
+    private static void SeedCountries(AuthenticatorDbContext context, IEnumerable<Country> countries)
+    {
+        if (context.Countries.Any()) return;
+        foreach (var country in countries)
+        {
+            country.CreatedOn = DateTime.UtcNow;
+            country.UpdatedOn = DateTime.UtcNow;
+        }
+
+        context.Countries.AddRange(countries);
+        context.SaveChanges();
+    }
+}
--- src/Authenticator.DbMigrator/Program.cs
+++ src/Authenticator.DbMigrator/Program.cs
@@ -1,3 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics.CodeAnalysis;
+using Authenticator.DbMigrator.Data;
+using Authenticator.DbMigrator.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-Console.WriteLine("Hello, World!");
+namespace Authenticator.DbMigrator;
+
+/// <summary>
+///     Provides for the application's hosting environment, configuration, and execution.
+/// </summary>
+[ExcludeFromCodeCoverage]
+public class Program
+{
+    static async Task Main(string[] args) => await CreateHostBuilder(args).Build().RunAsync();
+
+    /// <summary>
+    ///     Builds the application's host.
+    /// </summary>
+    /// <param name="args">A <see cref="string" />[] representing the application's arguments.</param>
+    /// <returns>An <see cref="IHostBuilder" /> representing the host builder.</returns>
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureServices(services =>
+            {
+                services.AddIdentitiesAndIdentityServer(GetConfigurationBuilder().Build());
+
+                var provider = services.BuildServiceProvider();
+                Seeds.InitializeIdentityDatabase(provider);
+                Seeds.InitializeAuthDatabase(provider);
+
+                Environment.Exit(0);
+            });
+
+    private static IConfigurationBuilder GetConfigurationBuilder() =>
+        new ConfigurationBuilder().AddJsonFile("appsettings.json").AddUserSecrets<Program>().AddEnvironmentVariables();
+}
