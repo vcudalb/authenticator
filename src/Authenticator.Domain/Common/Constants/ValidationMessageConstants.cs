98582eaaef27aa4c3cc23cc3cd5f4d9b1ec7424f
vcudalb <cudalb.vasile@gmail.com>
2023-10-07 22:42:48 +0300

    feat: create token request validator

---
+++ src/Authenticator.Domain/Common/Constants/ValidationMessageConstants.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Authenticator.Domain.Common.Constants;
+
+[ExcludeFromCodeCoverage]
+public class ValidationMessageConstants
+{
+        public const string NotEmptyValidationMessage = "{0} cannot be empty.";
+        public const string GreaterThanValidationMessage = "{0} should be greater than {1}.";
+        public const string LessThanValidationMessage = "{0} should be less than {1}.";
+        public const string BetweenValidationMessage = "{0} should be between {1} and {2}.";
+        public const string MinLenпthValidationMessage = "The length of {0} must be at least {1} characters.";
+        public const string MaxLengthValidationMessage = "The length of {0} must be {1} characters or fewer.";
+        public const string MatchesValidationMessage = "{0} must contain at least one {1}.";
+        public const string NotExistsValidationMessage = "{0} doesn't exists. Please provide a valid one.";
+        public const string InvalidModelValidationMessage = "Invalid {0}.";
+}
---
+++ src/Authenticator.Domain/Validation/Extensions/ValidatorServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Authenticator.Domain.Requests.Authenticators.Tokens;
+using Authenticator.Domain.Validation.Abstractions;
+using Authenticator.Domain.Validation.Validators;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Authenticator.Domain.Validation.Extensions;
+
+[ExcludeFromCodeCoverage]
+public static class ValidatorServiceCollectionExtensions
+{
+    public static IServiceCollection AddValidators(this IServiceCollection services)
+    {
+        services.AddTransient(typeof(IValidationFactory), typeof(ValidationFactory));
+        services.AddTransient<IValidator<CreateTokenRequest>, CreateTokenValidator>();
+
+        return services;
+    }
+}
