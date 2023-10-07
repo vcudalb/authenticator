a49ada9d3fb9065a174340d10a4d79f49ca76a39
vcudalb <cudalb.vasile@gmail.com>
2023-10-07 22:34:56 +0300

    feat: validation factory

---
+++ src/Authenticator.Domain/Validation/Abstractions/IValidationFactory.cs
@@ -0,0 +1,8 @@
+﻿using FluentValidation;
+
+namespace Authenticator.Domain.Validation.Abstractions;
+
+public interface IValidationFactory
+{
+    TValidator GetValidator<TValidator>() where TValidator : IValidator;
+}
---
+++ src/Authenticator.Domain/Validation/ValidationFactory.cs
@@ -0,0 +1,18 @@
+﻿using Authenticator.Domain.Validation.Abstractions;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Authenticator.Domain.Validation;
+
+public class ValidationFactory : IValidationFactory
+{
+    private readonly IServiceScopeFactory _serviceScopeFactory;
+
+    public ValidationFactory(IServiceScopeFactory serviceScopeFactory)
+    {
+        _serviceScopeFactory = serviceScopeFactory;
+    }
+
+    public TValidator GetValidator<TValidator>() where TValidator : IValidator =>
+        _serviceScopeFactory.CreateScope().ServiceProvider.GetService<TValidator>();
+}
